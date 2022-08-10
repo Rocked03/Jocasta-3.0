fc2fd9d8a84024b34b756b62342ca0a74fc248b1
Rocked03 <rockedohthree@gmail.com>
2022-08-10 12:09:52 +1200

    Updated main.py for d.py 2.0.0a

--- main.py
+++ main.py
@@ -6,41 +6,50 @@ from config import *
 import datetime
 
 
-description = "Jocasta"
-bot = commands.Bot(command_prefix=commands.when_mentioned_or(BOT_PREFIX), description=description)
+class Jocasta(commands.Bot):
+	async def setup_hook(self):
+		initial_extensions = [
+			'funcs.postgresql',
+			'cogs.owner',
+			'cogs.polls',
+		]
+
+		for extension in initial_extensions:
+			await bot.load_extension(extension)
 
 
-initial_extensions = [
-]
 
-if __name__ == '__main__':
-    for extension in initial_extensions:
-        bot.load_extension(extension)
+intents = discord.Intents.all()
+
+description = "Jocasta 3.0"
+BOT_PREFIX = "=="
+get_pre = lambda bot, message: "=="
+bot = Jocasta(command_prefix=get_pre, description=description, intents=intents)
 
 bot.recentcog = None
 
 @bot.event
 async def on_connect():
-    print('Loaded Discord')
-    activity = discord.Game(name="Starting up...")
-    await bot.change_presence(status=discord.Status.idle, activity=activity)
+	print('Loaded Discord')
+	activity = discord.Game(name="Starting up...")
+	await bot.change_presence(status=discord.Status.idle, activity=activity)
 
 @bot.event
 async def on_ready():
-    print('------')
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print(discord.utils.utcnow().strftime("%d/%m/%Y %I:%M:%S:%f"))
-    print('------')
+	print('------')
+	print('Logged in as')
+	print(bot.user.name)
+	print(bot.user.id)
+	print(discord.utils.utcnow().strftime("%d/%m/%Y %I:%M:%S:%f"))
+	print('------')
 
-    statusactivity = f"discord.gg/marvel | Type {BOT_PREFIX}help"
-    await bot.change_presence(activity=discord.Game(name=statusactivity), status=discord.Status.online)
+	# statusactivity = f"discord.gg/marvel | Type {BOT_PREFIX}help"
+	# await bot.change_presence(activity=discord.Game(name=statusactivity), status=discord.Status.online)
 
 
 @bot.check
 async def globally_block_dms(ctx):
-    return ctx.guild is not None
+	return ctx.guild is not None
 
 
 bot.run(TOKEN, reconnect=True)
