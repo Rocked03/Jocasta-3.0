ff7b7fb0b6bfb9913810fb64e12fdb9b4f1ea8ec
Rocked03 <rockedohthree@gmail.com>
2022-08-21 02:02:48 +1200

    getting rid of config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-TOKEN = "NDE4OTgyNjM1OTM5ODIzNjE4.D3l6WQ.PYUDp0VSDKqnSBtCBPvtLeVihLc"
-BOT_PREFIX = "=="
-
-marvelKey_public = "c308e081bf8a6f8347e89da166b8be55"
-marvelKey_private = "13f8386ead310c3da88b68be2b5088034dbfa60b"
