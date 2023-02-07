ea6d90cd015cfe877ed6213d0e8d0b11e39e3c25
Rocked03 <rockedohthree@gmail.com>
2023-02-08 01:13:42 +1300

    Fun cog!

---
+++ cogs/fun.py
@@ -0,0 +1,29 @@
+import discord
+from discord import *
+from discord.ext.commands import *
+from discord.app_commands import *
+from discord.app_commands.tree import _log
+
+
+class FunCog(Cog, name="Fun"):
+    """Fun commands"""
+
+    def __init__(self, bot):
+        self.bot: Bot = bot
+
+        self.bot.tree.on_error = self.on_app_command_error
+
+        self.shrink = str.maketrans("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_",
+                                    "⁰¹²³⁴⁵⁶⁷⁸⁹ᵃᵇᶜᵈᵉᶠᵍʰⁱʲᵏˡᵐⁿᵒᵖᵠʳˢᵗᵘᵛʷˣʸᶻᵃᵇᶜᵈᵉᶠᵍʰⁱʲᵏˡᵐⁿᵒᵖᵠʳˢᵗᵘᵛʷˣʸᶻ‾")
+
+    async def on_app_command_error(self, interaction: Interaction, error: AppCommandError):
+        await interaction.followup.send("Something broke!")
+        _log.error('Ignoring exception in command %r', interaction.command.name, exc_info=error)
+
+    @app_commands.command(name="shrinktext")
+    async def shrinktext(self, interaction: discord.Interaction, text: str, show: bool = False):
+        """Shrink your text!"""
+        await interaction.response.send_message(text.translate(self.shrink), ephemeral=not show)
+
+async def setup(bot):
+    await bot.add_cog(FunCog(bot))
